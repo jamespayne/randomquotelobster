79343bdb45baa4dd22c1b83c3367d7ad5b5ca713
James Payne <jpayne001@me.com>
2016-12-29 14:47:42 +1100

    Updated code to include link to author in wikipedia.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  
+
   getQuote();
 
   function getQuote() {
@@ -7,38 +7,47 @@ $(document).ready(function() {
     var quotetext = "";
     var author = "";
     var quote = "";
-    
+    var wikiauthor = "";
+
     // Clear the quote div contents on load.
 
     $("#quote").html();
-    
+
     $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(json) {
+
       quote += json.quoteText;
       author += json.quoteAuthor;
+      wikiauthor = author.replace(/ /, '+');
 
-      if (author.length == 0) {
-        quotetext = quote + '- ' + ' Unknown';
-        $("#quote").html('<p>'+ quotetext +'</p>');
+      if (author.length === 0) {
+        quotetext = quote + ' - ' + ' Unknown';
+        $("#quote").html('<p>' + quotetext);
       } else {
-        quotetext = quote + '- ' + author;
-        $("#quote").html('<p>' + '"' + quote + '"' + '</i> - ' + '<b>' + author + '</p>');
+        quotetext = quote;
+        $("#quote").html('<p>' + '"' + quote + '" ' + "- " + '<a href="" id="author">' + author + '</a></p>');
       }
-      
+
       // Set up the twitter button URL.
-      
-      var twitterurl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(quotetext);
+
+      var twitterurl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(quotetext);
       $('#twitter-share').attr("href", twitterurl);
+
+      // Query wikipedia and see if there is an article about the author and provide a link if possible. This needs some error handling as there may not be a page or the author is unknown.
+
+      $.getJSON('https://en.wikipedia.org//w/api.php?action=query&origin=*&format=json&prop=info&meta=&titles=' + wikiauthor + '&redirects=1&formatversion=latest&inprop=url', function(json) {
+        $("#author").attr('href', json.query.pages[0].fullurl)
+      });
 
     });
 
   }
-  
+
   // Click the lobster for another quote after the first quote has been loaded.
 
   $("#lobster").on("click", function() {
     getQuote();
   });
-  
+
   $('[data-toggle="tooltip"]').tooltip();
 
 });
